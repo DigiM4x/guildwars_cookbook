7208fd9f540ac747820e4e1ebc0ac1f68eb77d7c
Peter <parkper@gmail.com>
2012-10-09 02:19:32 -0400

    wrote in CRUD for ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,4 +1,36 @@
-class PagesController < ApplicationController
+class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredients.all
+  end
 
+  def new
+    @ingredient = Ingredients.new
+  end
+
+  def create
+    @ingredient = Ingredient.create(params[:ingredient])
+    if @user.save
+      flash[:notice] = "Ingredient successflly created"
+      render :action => :show
+    else
+      render :action => :edit
+    end
+  end
+
+  def update
+    @ingredient.update_attributes(params[:id])
+    if @user.valid?
+      flash[:notice] = "Ingredient successfully updated"
+      render :action => :show
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+  end
+
+  def show
+  end
 
 end
